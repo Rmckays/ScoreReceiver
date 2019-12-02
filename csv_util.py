d52ba568c36585819d03f14621a9570150f7db67
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-12-01 21:05:20 -0700

    Update for statement when reading csv file.

--- csv_util.py
+++ csv_util.py
@@ -2,3 +2,9 @@ import csv
 
 with open('scores.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
+
+    next(csv_reader)
+
+    for line in csv_reader:
+        if int(line[1]) > 2017 and (line[4] == "KC" or line[5] == "KC"):
+            print(line)
