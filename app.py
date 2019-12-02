3af89c460e27451e621fb57ee6ee84ee113338b1
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-12-01 20:30:32 -0700

    update static folder servering.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='build/static', template_folder='./client/git build')
+app = Flask(__name__, static_folder='./client/build/static', template_folder='./client/build')
 
 @app.route('/')
 def home():
