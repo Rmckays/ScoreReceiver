76f0ccab004a219101f91849f4f169d5cd47cf2c
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-12-01 20:24:15 -0700

    Create initial flask server.

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='build/static', template_folder='./client/git build')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+print('Starting Flask!')
+
+app.run(port=4000)
