8d4dda6136fff01bb7c08912ea76f9cbee60bae2
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-12-03 18:15:20 -0700

    Add route to return score json.

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+
+import csv_util
 
 app = Flask(__name__, static_folder='./client/build/static', template_folder='./client/build')
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/api/scores/<string:name>', methods=['GET'])
+def send_scores(name):
+    return jsonify(csv_util.return_data(name))
 
 
 print('Starting Flask!')
--- csv_util.py
+++ csv_util.py
@@ -1,10 +1,16 @@
 import csv
 
-with open('scores.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
 
-    next(csv_reader)
+def return_data(name):
+    data = {
+        'scores': []
+    }
+    with open('scores.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
 
-    for line in csv_reader:
-        if int(line[1]) > 2017 and (line[4] == "KC" or line[5] == "KC"):
-            print(line)
+        next(csv_reader)
+
+        for line in csv_reader:
+            if int(line[1]) > 2017 and (line[4] == name.upper() or line[5] == name.upper()):
+                data['scores'].append(line)
+    return data
