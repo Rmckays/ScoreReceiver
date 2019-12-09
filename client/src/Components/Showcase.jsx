d851f2a06b888c51c64a4a16752eeaa37599970a
Rustyn Simmons <Rustyn@rustynsimmons.com>
2019-12-09 14:47:21 -0700

    Add navigation and showcase components.

---
+++ client/src/Components/Showcase.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Components.modules.css';
+
+const Showcase = () => {
+    return (
+        <div className="backdrop">
+
+        </div>
+    )
+};
+
+export default Showcase;
