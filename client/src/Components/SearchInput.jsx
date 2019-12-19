51d3168cd8b5752a2b98b1d0bc39c94f681e90f6
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-12-18 20:41:51 -0700

    Add map team list to select input.

--- client/src/Components/SearchInput.jsx
+++ client/src/Components/SearchInput.jsx
@@ -1,12 +1,36 @@
 import React, {useEffect} from 'react';
 import agent from '../API/agent';
 import {connect} from "react-redux";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const SearchInput = props => {
     useEffect(() => {
         agent.teams.getTeams()
             .then(response => props.loadTeams(response))
-    }, [])
+    }, []);
+
+    const handleChange = event => {
+        props.changeTeam(event);
+    };
+
+    const teamList = props.teams.map(team => {
+        return(
+            <MenuItem value={team}>{team}</MenuItem>
+        )
+    });
+
+    return (
+        <div className='searchInput'>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              onChange={handleChange}
+            >
+                {teamList}
+            </Select>
+        </div>
+    )
 
 };
 
