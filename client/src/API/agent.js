7703968431947daec6495cda7d10bebf7e21e5dd
Rustyn Simmons <rustyn@rustynsimmons.com>
2019-12-18 20:13:28 -0700

    Add axios agent and SearchInput component.

---
+++ client/src/API/agent.js
@@ -0,0 +1,19 @@
+import axios, {AxiosResponse} from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:5000/api';
+
+const responseBody = response => response.data;
+
+const requests = {
+    get: url => axios.get(url).then(responseBody)
+};
+
+const getStats = {
+    getTeamHistory: (team, year) => requests.get(`/history/${team}/${year}`),
+    getTeamSeason: (team, season) => requests.get(`/season/${team}/${season}`)
+};
+
+const teams = {
+    getTeams: value => requests.get(`/teams`)
+};
+
---
+++ client/src/Components/SearchInput.jsx
@@ -0,0 +1,10 @@
+import React, {useEffect} from 'react';
+
+const SearchInput = () => {
+    useEffect(() => {
+
+    }, [])
+
+}
+
+export default SearchInput;
