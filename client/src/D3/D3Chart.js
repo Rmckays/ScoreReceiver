8ee6915d4124ccb4c76ce600737d1208edb65623
Rustyn Simmons <Rustyn@rustynsimmons.com>
2020-01-03 16:46:37 -0700

    Add axis for D3Chart. Refactor code.

--- client/src/D3/D3Chart.js
+++ client/src/D3/D3Chart.js
@@ -1,14 +1,17 @@
 import * as d3 from 'd3';
 
-const HEIGHT = 500;
-const WIDTH = 1000;
+const MARGINS = {top: 10, right: 20, left: 20, bottom: 20};
+const HEIGHT = 500 - MARGINS.left - MARGINS.right;
+const WIDTH = 1000 - MARGINS.top - MARGINS.bottom;
 
 export default class D3Chart {
     constructor(element, data, currentTeam) {
         const svg = d3.select(element)
             .append("svg")
-            .attr("width", WIDTH)
-            .attr("height", HEIGHT);
+                .attr("width", WIDTH + MARGINS.left + MARGINS.right)
+                .attr("height", HEIGHT + MARGINS.top + MARGINS.bottom)
+            .append("g")
+                .attr("transform", `translate(${MARGINS.left}, ${MARGINS.top})`);
 
         const maxAway = d3.max(data, d => {
             return parseInt(d.away_score);
@@ -22,12 +25,20 @@ export default class D3Chart {
 
         const y = d3.scaleLinear()
             .domain([0, absMax])
-            .range([0, HEIGHT]);
+            .range([HEIGHT, 0]);
 
         const x = d3.scaleBand()
             .domain(data.map(game => game.date))
             .range([0, WIDTH])
-            .padding(0.25);
+            .padding(0.4);
+
+        const xAxisCall = d3.axisBottom(x);
+        svg.append("g")
+            .attr("transform", `translate(0, ${HEIGHT})`).call(xAxisCall);
+
+        const yAxisCall = d3.axisLeft(y);
+        svg.append("g").call(yAxisCall);
+
 
         const rects = svg.selectAll("rect")
             .data(data);
@@ -36,17 +47,17 @@ export default class D3Chart {
             .attr("x", d => x(d.date))
             .attr("y", d => {
                 if(d.home_team === currentTeam){
-                    return HEIGHT - y(d.home_score)
+                    return y(d.home_score)
                 } else {
-                    return HEIGHT - y(d.away_score)
+                    return y(d.away_score)
                 }
             })
             .attr("width", x.bandwidth)
             .attr("height", d => {
                 if(d.home_team === currentTeam){
-                    return y(d.home_score)
+                    return HEIGHT - y(d.home_score);
                 } else {
-                    return y(d.away_score)
+                    return HEIGHT - y(d.away_score);
                 }
                 })
             .attr("fill", d => {
