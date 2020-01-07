26d71154bec4399c0f102b3ce9eea6cf68baeb2a
Rustyn Simmons <Rustyn@rustynsimmons.com>
2020-01-06 21:35:43 -0700

    Update build folder.

---
+++ client/build/precache-manifest.5dde8f4309b9c555f1afcadfd2b9b13b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130b025d46f3314000c947ec39e8e086",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef7f7c99df97a2bea124",
+    "url": "/static/css/main.f37f53df.chunk.css"
+  },
+  {
+    "revision": "b5acb0c0ce03d739c356",
+    "url": "/static/js/2.d7117b78.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/static/js/2.d7117b78.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef7f7c99df97a2bea124",
+    "url": "/static/js/main.8749f685.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "606faf8ea139bb0c2e05e3295ab9d220",
+    "url": "/static/media/featured.606faf8e.jpg"
+  }
+]);
